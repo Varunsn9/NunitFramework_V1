3c845b8a6135b876c6b65488e810431fc9a347a4
Varunsn9 <varunsn9@gmail.com>
2023-08-31 13:16:51 +0530

    Added txt doc to test using testlist nunit cmd

---
+++ NunitFramework_V1/TestIng_Concepts/FetchingDataFromResult/TestScriptListeners.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitFramework_V1.TestIng_Concepts.FetchingDataFromResult
+{
+    [TestFixture]
+    [Category("Listeners")]
+    public class TestScriptListeners 
+    {
+        [Test]
+        public void TestingListeners()
+        {
+            TestContext.WriteLine($"the result is {TestContext.CurrentContext.Result}");
+        }
+        [Test]
+        public void TestingListeners0()
+        {
+            TestContext.WriteLine($"the result is {TestContext.CurrentContext.Result}");
+        }
+        [Test]
+        public void TestingListeners1()
+        {
+            TestContext.WriteLine($"the result is {TestContext.CurrentContext.Result}");
+        }
+    }
+}
