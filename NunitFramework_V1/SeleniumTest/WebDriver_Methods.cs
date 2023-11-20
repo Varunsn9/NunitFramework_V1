aca68fbd34957210c84edce5448a3603e6cfdd6e
Varunsn9 <varunsn9@gmail.com>
2023-11-20 12:50:52 +0530

    new

---
+++ NunitFramework_V1/SeleniumTest/WebDriver_Methods.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace NunitFramework_V1.SeleniumTest
+{
+    [TestFixture]
+    [Category("WebDriverMethodsAndProperties")]
+    public class WebDriver_Methods
+    {
+        IWebElement element;
+        IWebDriver driver;
+        string url = "https://www.google.com";
+        [SetUp]
+        public void Launching_The_Browser()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+        }
+        
+        [Test]
+        [Category("WedDriverProperties")]
+        public void WedDriverProperties()
+        {
+            driver.Navigate().GoToUrl(url);
+
+            string uRL = driver.Url;
+            string pageSource = driver.PageSource;
+            string currentWindowHandle = driver.CurrentWindowHandle;
+            string title=driver.Title;
+            ReadOnlyCollection<string> windowHandles=driver.WindowHandles;
+            Console.WriteLine("url = "+uRL);
+            Console.WriteLine("pageSource = "+pageSource);
+            Console.WriteLine("Currentwindowhandles = " + currentWindowHandle);
+            Console.WriteLine("title = " + title);
+            foreach (string windowHandle in windowHandles)
+            {
+                Console.WriteLine("windowshandles = " + windowHandle);
+            }
+        }
+        [Test]
+        [Category("WebDriverMethods")]
+        public void WebDriverMethods()
+        {
+            var displayed = element.Displayed;
+            var enabled=element.Enabled;
+            var size=element.Size;
+            var text=element.Text;
+            var location=element.Location;
+            var selected=element.Selected;
+            var tagName = element.TagName;
+            element.Clear();
+            element.Click();
+            element.GetAttribute("string attribute");
+            element.GetCssValue("get css value");
+            element.Submit();
+            element.GetProperty("propertyName");
+            element.SendKeys("");
+            
+            
+        }
+
+        [TearDown]
+        public void ClosingTheBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ NunitFramework_V1/SeleniumTest/WebElementsMethods.cs
@@ -0,0 +1,261 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Opera;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace NunitFramework_V1.SeleniumTest
+{
+    [TestFixture]
+    [Category("WebElement")]
+    public class WebElementsMethods
+    {
+
+        string url = "https://demo.actitime.com";
+        IWebDriver driver;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            new DriverManager().SetUpDriver(new OperaConfig());
+            driver = new OperaDriver();
+        }
+
+        [Test]
+        [Category("WebElement_Text")]
+        public void Text()
+        {
+            driver.Navigate().GoToUrl(url);
+            IWebElement element= driver.FindElement(By.TagName("td"));
+            Console.WriteLine(element.Text);
+            /* 
+             * if there is no text in the element it will return empty
+             * if a locater finds multiple element it will return all the text 
+             * if there is wrong address then it will return NoSuchElementException
+             * return type is string
+             */
+        }
+
+        [Test]
+        [Category("WebElement_TagName")]
+        public void TagName()
+        {
+            driver.Navigate().GoToUrl(url);
+            IWebElement element = driver.FindElement(By.Name("username"));
+            Console.WriteLine(element.TagName);
+            /* 
+             * if there is wrong address then it will return NoSuchElementException
+             * return type is string
+             */
+        }
+
+        [Test]
+        [Category("WebElement_Location")]
+        public void Location()
+        {
+            driver.Navigate().GoToUrl(url);
+            IWebElement element = driver.FindElement(By.Name("username"));
+            Console.WriteLine(element.Location);
+            /* 
+             * if there is wrong address then it will return NoSuchElementException
+             * return type is Point
+             */
+        }
+
+        [Test]
+        [Category("WebElement_Displayed")]
+        public void Displayed()
+        {
+            driver.Navigate().GoToUrl(url);
+            IWebElement element = driver.FindElement(By.Name("username"));
+            Console.WriteLine(element.Displayed);
+            /* 
+             * if there is wrong address then it will return NoSuchElementException
+             * return type is bool
+             */
+        }
+
+        [Test]
+        [Category("WebElement_Enabled")]
+        public void Enabled()
+        {
+            driver.Navigate().GoToUrl(url);
+            IWebElement element = driver.FindElement(By.Name("username"));
+            Console.WriteLine(element.Enabled);
+            /* 
+             * if there is wrong address then it will return NoSuchElementException
+             * return type is bool
+             */
+        }
+
+        [Test]
+        [Category("WebElement_Selected")]
+        public void Selected()
+        {
+            driver.Navigate().GoToUrl(url);
+            IWebElement element = driver.FindElement(By.Name("remember"));
+            //driver.FindElement(By.Name("remember")).Click();
+            Console.WriteLine(element.Selected);
+            /* 
+             * if there is wrong address then it will return NoSuchElementException
+             * return type is bool
+             */
+        }
+
+        [Test]
+        [Category("WebElement_Size")]
+        public void Size()
+        {
+            driver.Navigate().GoToUrl(url);
+            IWebElement element = driver.FindElement(By.Name("remember"));
+            Console.WriteLine(element.Size);
+            /* 
+             * if there is wrong address then it will return NoSuchElementException
+             * return type is bool
+             */
+        }
+
+        [Test]
+        [Category("WebElement_Click")]
+        public void Click()
+        {
+            driver.Navigate().GoToUrl(url);
+            IWebElement element = driver.FindElement(By.Id("toPasswordRecoveryPageLink"));
+            element.Click();
+            /* 
+             * if there is wrong address then it will return NoSuchElementException
+             * return type is void
+             */
+        }
+
+        [Test]
+        [Category("WebElement_Clear")]
+        public void Clear()
+        {
+            driver.Navigate().GoToUrl(url);
+            IWebElement element = driver.FindElement(By.Id("username"));
+            element.SendKeys("manager");
+            Thread.Sleep(3000);
+            element.Clear();
+            /* 
+             * if there is wrong address then it will return NoSuchElementException
+             * return type is void
+             */
+        }
+
+        [Test]
+        [Category("WebElement_GetCssValue")]
+        public void GetCssValue()
+        {
+            driver.Navigate().GoToUrl(url);
+            IWebElement element = driver.FindElement(By.Id("username"));
+            string cssValue= element.GetCssValue("width");
+            Console.WriteLine(cssValue);
+            
+            /* 
+             * if there is wrong address then it will return NoSuchElementException
+             * return type is string
+             */
+        }
+
+        [Test]
+        [Category("WebElement_GetAttribute")]
+        public void GetAttribute()
+        {
+            driver.Navigate().GoToUrl(url);
+            IWebElement element = driver.FindElement(By.Id("username"));
+            string getAttribute = element.GetAttribute("class");
+            Console.WriteLine(getAttribute);
+
+            /* 
+             * if there is wrong address then it will return NoSuchElementException
+             * return type is string
+             */
+        }
+
+
+        [Test]
+        [Category("WebElement_Submit")]
+        public void Submit()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("hello");
+            element.Submit();
+
+            /* 
+             * if there is wrong address then it will return NoSuchElementException
+             * return type is void
+             */
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Thread.Sleep(5000);
+            driver.Close();
+            driver.Dispose();
+        }
+    }
+    [TestFixture]
+    public class DataStructure
+    {
+        [Test,Category("ll")]
+        public void LinkedListTesting()
+        {
+            LinkedList<string> ll=new LinkedList<string>();
+            ll.AddLast("8");
+            ll.AddFirst("0");
+            var find=ll.Find("8");
+            ll.AddAfter(find, "9");
+            ll.AddBefore(find, "7");
+            
+            
+            foreach (string s in ll)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        [Test,Category("AsyncAndAwait")]
+        public void AsyncAndAwaittest1()
+        {
+            Method1();
+            Method2();
+            //Console.ReadKey();
+        }
+
+        public static async Task A()
+        {
+            await Task.Delay(1000);
+            await Console.Out.WriteLineAsync("using await writeline async");
+            await Task.Delay(1000);
+        }
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                    // Do something
+                    Task.Delay(100).Wait();
+                }
+            });
+        }
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(" Method 2");
+                // Do something
+                Task.Delay(100).Wait();
+            }
+        }
+    }
+}
